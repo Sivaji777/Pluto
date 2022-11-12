ada908f717736864fefd74a9ee370060b3da2068
usman <haroonusman046@gmail.com>
2022-11-12 12:00:30 +0530

    updated

--- Pluto/src/Components/Common/test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'; 
-import ScriptTag from 'react-script-tag';
-
-const Userdetailsscript = ()=>{
-	return(
-		<ScriptTag type="text/javascript" src="user-details.js"></ScriptTag>
-	);
-}
-export default Userdetailsscript;
